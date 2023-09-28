50f98739e60a9ab2d97fe17bb0e120493561444f
mateotamayoo <al.christian.tamayo.rivera@iesportada.org>
2023-09-28 08:47:47 +0200

    Cambio gitignore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,6 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.8.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation(files("C:\\Users\\Mateo Tamayo\\AppData\\Local\\Android\\Sdk\\platforms\\android-34\\android.jar"))
     implementation(files("C:\\Users\\Mateo Tamayo\\AppData\\Local\\Android\\Sdk\\platforms\\android-33\\android.jar"))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
